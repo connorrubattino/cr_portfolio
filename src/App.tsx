634699603a58a2e83a1927da65a286e53938e70d
Connor Rubattino <connor.rubattino@gmail.com>
2024-05-28 14:42:25 -0700

    setup nav and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,23 @@
+import { Container } from "react-bootstrap";
+import Navigation from "./components/Navigation";
+import { Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+
+
+
 export default function App(){
-  return <h1>Hello world</h1>
+  
+  
+  return (
+    <>
+    <Navigation/>
+    <Container>
+        <Routes>
+          {/* <Route path='/' element={<Home/>} /> */}
+        </Routes>
+    </Container>
+    <Footer/>
+    </>
+  )
+
 }
