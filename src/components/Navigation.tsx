1fdce4c3f1399035f534d95fc29b97e4dacf2475
Connor Rubattino <connor.rubattino@gmail.com>
2024-05-29 14:38:52 -0700

    navbar hover

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,12 +2,16 @@ import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { useState } from 'react';
 
 type Props = {}
 
 export default function Navigation({}: Props) {
+
+    const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <Navbar expand='lg' style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}>
+    <Navbar expand='lg' style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }} className={isHovered ? 'hovered' : ''}>
             <Container fluid>
                 <Navbar.Brand as={Link} to='/'>
                     <img
@@ -22,9 +26,9 @@ export default function Navigation({}: Props) {
                 <Navbar.Collapse id='nav-collapse'>
                     <Nav className='me-auto'>
                             <>
-                                <Nav.Link className='text-white navbar-link ms-3' style={{ fontWeight: 'bold' }} as={Link} to='/portfolio'>Portfolio</Nav.Link>
-                                <Nav.Link className='text-white navbar-link ms-3' style={{ fontWeight: 'bold' }} as={Link} to='/about'>About Me</Nav.Link>
-                                <Nav.Link className='text-white navbar-link ms-3' style={{ fontWeight: 'bold' }} as={Link} to='/resume'>Resume</Nav.Link>
+                                <Nav.Link onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className='text-white navbar-link ms-5' style={{ fontWeight: 'bold' }} as={Link} to='/portfolio'>Portfolio</Nav.Link>
+                                <Nav.Link onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className='text-white navbar-link ms-5' style={{ fontWeight: 'bold' }} as={Link} to='/about'>About Me</Nav.Link>
+                                <Nav.Link onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} className='text-white navbar-link ms-5' style={{ fontWeight: 'bold' }} as={Link} to='/resume'>Resume</Nav.Link>
                             </>
                         
                     </Nav>
